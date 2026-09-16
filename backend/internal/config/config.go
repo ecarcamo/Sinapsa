599d1cbaa879302ac5f548e03f92bb00236bd852
ecarcamo <estebancarcamou@gmail.com>
2026-09-15 18:47:11 -0600

    [feat] support DATABASE_URL for cloud database environments

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -6,30 +6,35 @@ import (
 )
 
 type Config struct {
-	Port       string
-	AppEnv     string
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
-	DBSSLMode  string
+	DatabaseURL string
+	Port        string
+	AppEnv      string
+	DBHost      string
+	DBPort      string
+	DBUser      string
+	DBPassword  string
+	DBName      string
+	DBSSLMode   string
 }
 
 func LoadConfig() *Config {
 	return &Config{
-		Port:       getEnv("PORT", "8080"),
-		AppEnv:     getEnv("APP_ENV", "development"),
-		DBHost:     getEnv("DB_HOST", "localhost"),
-		DBPort:     getEnv("DB_PORT", "5432"),
-		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPassword: getEnv("DB_PASSWORD", "postgres"),
-		DBName:     getEnv("DB_NAME", "sinapsa_db"),
-		DBSSLMode:  getEnv("DB_SSLMODE", "disable"),
+		DatabaseURL: getEnv("DATABASE_URL", ""),
+		Port:        getEnv("PORT", "8080"),
+		AppEnv:      getEnv("APP_ENV", "development"),
+		DBHost:      getEnv("DB_HOST", "localhost"),
+		DBPort:      getEnv("DB_PORT", "5432"),
+		DBUser:      getEnv("DB_USER", "postgres"),
+		DBPassword:  getEnv("DB_PASSWORD", "postgres"),
+		DBName:      getEnv("DB_NAME", "sinapsa_db"),
+		DBSSLMode:   getEnv("DB_SSLMODE", "disable"),
 	}
 }
 
 func (c *Config) GetDSN() string {
+	if c.DatabaseURL != "" {
+		return c.DatabaseURL
+	}
 	return fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=UTC",
 		c.DBHost, c.DBUser, c.DBPassword, c.DBName, c.DBPort, c.DBSSLMode,
